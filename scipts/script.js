8c0aa3f3ae8e3bc4b4a01b492804c108a744eff5
Ryan Kim <hanjun1@ualberta.ca>
2020-12-30 14:03:28 -0700

    added grayscale

--- scipts/script.js
+++ scipts/script.js
@@ -1,4 +1,5 @@
 const mainContainer = document.querySelector("#main-container");
+const allButtons = document.querySelectorAll("button");
 let gridSize = 16;
 let backgroundColor = 'default';
 
@@ -12,23 +13,44 @@ function createGrid(n) {
 }
 
 function reset(e) {
-    while (mainContainer.firstChild) {
-        mainContainer.removeChild(mainContainer.lastChild);
+    while (true) {
+        let gridSize = parseInt(prompt("Enter the grid size (n) you want: (n x n) (Maximum n value is 100)"));
+        console.log(gridSize);
+        if (gridSize <= 100 && gridSize >0) {
+            while (mainContainer.firstChild) {
+                mainContainer.removeChild(mainContainer.lastChild);
+            }
+            mainContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
+            mainContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
+            backgroundColor = "default";
+            main(gridSize);
+            break;
+        } else if (isNaN(gridSize)) {
+            break;
+        }
     }
-    let gridSize = parseInt(prompt("Enter the grid size (n) you want: (n x n) (Maximum n value is 100)"));
-    mainContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`
-    mainContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`
-    main(gridSize);
+    
 } 
 
 const resetButton = document.querySelector("#reset");
 resetButton.addEventListener('click', reset);
+
+function changeGrayscale(e) {
+    clearButtons();
+    grayscaleButton.classList.add("btn-pressed");
+    backgroundColor = "grayscale";
+}
+
+const grayscaleButton = document.querySelector("#grayscale");
+grayscaleButton.addEventListener('click', changeGrayscale);
 
 function randomNumberRGB() {
     return Math.floor(Math.random() * 255);
 }
 
 function changeColorRandom(e) {
+    clearButtons();
+    randomColorButton.classList.add("btn-pressed");
     backgroundColor = "rgb";
 }
 
@@ -36,6 +58,8 @@ const randomColorButton = document.querySelector("#rgb-color");
 randomColorButton.addEventListener('click', changeColorRandom);
 
 function defaultColor() {
+    clearButtons();
+    defaultButton.classList.add("btn-pressed");
     backgroundColor = 'default';
 }
 
@@ -51,15 +75,29 @@ function rgbColor() {
 }
 
 function chooseColor(e) {
+    clearButtons();
+    colorChooseButton.classList.add("btn-pressed");
     backgroundColor = "choose-color";
 }
 
 const colorChooseButton = document.querySelector("#choose-color");
 colorChooseButton.addEventListener('click', chooseColor);
 
+function clearButtons() {
+    allButtons.forEach((button) => {
+      if (button.classList.contains("btn-pressed")) {
+        button.classList.remove("btn-pressed");
+        button.classList.add("btn-not-pressed");
+      }
+    })
+}
+
 function main(gridSize) {
 
     createGrid(gridSize);
+
+    clearButtons();
+    defaultButton.classList.add("btn-pressed");
 
     function changeColor(e) {
         if (backgroundColor === "rgb") {
@@ -68,6 +106,17 @@ function main(gridSize) {
             e.target.style.backgroundColor = "black";
         } else if (backgroundColor === "choose-color") {
             e.target.style.backgroundColor = document.getElementById("color-picker").value;
+        } else if (backgroundColor === "grayscale") {
+            console.log(e.target.style.backgroundColor.match(/rgba/));
+            if (e.target.style.backgroundColor.match(/rgba/)) {
+                let currentGrayscale = parseFloat(e.target.style.backgroundColor.slice(-4,-1));
+                console.log(currentGrayscale);
+                if (currentGrayscale < 1.0) {
+                    e.target.style.backgroundColor = `rgba(0,0,0,${currentGrayscale+0.1})`;
+                }
+            } else {
+                e.target.style.backgroundColor = "rgba(0,0,0,0.1)";
+            }
         }
     }
     
